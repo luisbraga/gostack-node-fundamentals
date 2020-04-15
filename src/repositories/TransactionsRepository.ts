884ba7b09238d8d963ddbce66a2cac66bcc015ba
luisbraga <luisbraga05@gmail.com>
2020-04-15 23:48:18 +0100

    Implements all test features

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,4 +1,16 @@
+// import { uuid } from 'uuidv4'
 import Transaction from '../models/Transaction';
+
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+interface TransactionsWithBalance {
+  transactions: Transaction[];
+  balance: Balance;
+}
 
 interface Balance {
   income: number;
@@ -13,16 +25,34 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): TransactionsWithBalance {
+    return {
+      transactions: [...this.transactions],
+      balance: this.getBalance(),
+    };
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = this.transactions.reduce(
+      (acc, { type, value }) => {
+        const income = type === 'income' ? value : 0;
+        const outcome = type === 'outcome' ? value : 0;
+        return {
+          income: acc.income + income,
+          outcome: acc.outcome + outcome,
+          total: acc.income + income - (acc.outcome + outcome),
+        };
+      },
+      { income: 0, outcome: 0, total: 0 },
+    );
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
